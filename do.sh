95966a4192a0b61d03bc4a3d6cbedcae22c33d7a
yingshaoxo <yingshaoxo@gmail.com>
2017-06-27 19:31:58 +0800

    I did what I should do, then all depends on you.

--- do.sh
+++ do.sh
@@ -1,5 +1,9 @@
-#export http_proxy=http://127.0.0.1:1088
-#export https_proxy=http://127.0.0.1:1088
-#pkill python
+#export http_proxy=http://127.0.0.1:1080
+
+pkill python
 python3 server.py &
 python3 telegram.py &
+
+#nohup python3 server.py &
+#nohup python3 server.py &
+#clear
